18221b9d4459d8b04cfeac702e983c60dad6f0c3
Abhishek Desai <55031453+abhidesai6@users.noreply.github.com>
2020-08-02 02:03:11 +0530

    Image exchange

--- lib/utils/utilities.dart
+++ lib/utils/utilities.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
-import 'package:image/image.dart';
+import 'dart:math';
+import 'package:image/image.dart' as Im;
 import 'package:flutter/cupertino.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
 
 class Utils {
   static String getUsername(String email) {
@@ -16,9 +18,22 @@ class Utils {
   }
 
   static Future<File> pickImage({@required ImageSource source}) async {
+    // ignore: deprecated_member_use
     File selectedImage = await ImagePicker.pickImage(source: source);
-    return selectedImage;
+    return compressImage(selectedImage);
   }
 
-  static Future<File> compressImage(File imageToCompress) async {}
+  static Future<File> compressImage(File imageToCompress) async {
+    final tempDir = await getTemporaryDirectory();
+
+    final path = tempDir.path;
+
+    int random = Random().nextInt(1000);
+
+    Im.Image image = Im.decodeImage(imageToCompress.readAsBytesSync());
+    Im.copyResize(image, width: 500, height: 500);
+
+    return new File('$path/img_$random.jpg')
+      ..writeAsBytesSync(Im.encodeJpg(image, quality: 85));
+  }
 }
